eba5aa372ef643beaa5f4d24cc0e7458908c32e0
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2015-08-04 17:32:00 +0200

    [FIX] Add expand button

--- web_widget_iframe/static/src/js/widget.js
+++ web_widget_iframe/static/src/js/widget.js
@@ -8,9 +8,22 @@ openerp.web_widget_iframe = function (instance) {
     instance.web.form.FieldIFrame = instance.web.form.FieldChar.extend({
         template: 'FieldIFrame',
         widget_class: 'oe_form_field_iframe',
+
         render_value: function () {
             var show_value = this.format_value(this.get('value'), '');
             this.$el.find('iframe').attr('src', show_value);
+        },
+
+        initialize_content: function() {
+            var self = this;
+            this.$('.oe_iframe_switch').click(function() {
+                self.$el.toggleClass('oe_iframe_fullscreen');
+                self.$el.find('.oe_iframe_switch').toggleClass('fa-expand fa-compress');
+                self.view.$el.find('.oe_chatter').toggle();
+                $('#oe_main_menu_navbar').toggle();
+            });
+            this.render_value();
         }
+
     });
 };
