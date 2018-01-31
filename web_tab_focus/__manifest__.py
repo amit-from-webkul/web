85e08a7e431544ace5551eb2e80ac2123ea8e2f7
andreparames <github@andreparames.com>
2018-01-31 12:09:40 +0100

    [10.0][ADD] web_tab_focus

---
+++ web_tab_focus/__manifest__.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+# Copyright 2018 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': "Web Tab Focus",
+    'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
+    'website': "https://odoo-community.org",
+    'category': 'web',
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'depends': [
+        'web',
+    ],
+    'data': [
+        'views/templates.xml',
+    ],
+    "demo": [
+    ],
+    'application': False,
+}
